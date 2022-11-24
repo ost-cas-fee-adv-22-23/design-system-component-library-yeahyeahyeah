e85e916fb0476aad5c185652cb65fd1a315af139
Thomas Schallert <thomas.schallert@fhnw.ch>
2022-11-24 19:14:25 +0100

    add git workflow

--- src/stories/button/Button.stories.tsx
+++ src/stories/button/Button.stories.tsx
@@ -19,6 +19,8 @@ Primary.args = {
   fCallBack: () => {
     console.log('callback primary');
   },
+  styles:
+    'text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800',
 };
 
 export const Secondary = Template.bind({});
@@ -27,6 +29,8 @@ Secondary.args = {
   fCallBack: () => {
     console.log('callback sec');
   },
+  styles:
+    'text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800',
 };
 
 export const Large = Template.bind({});
@@ -35,6 +39,8 @@ Large.args = {
   fCallBack: () => {
     console.log('callback large');
   },
+  styles:
+    'text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800',
 };
 
 export const Small = Template.bind({});
@@ -43,4 +49,6 @@ Small.args = {
   fCallBack: () => {
     console.log('callback small');
   },
+  styles:
+    'text-white bg-violet-700 hover:bg-violet-800 focus:ring-4 focus:outline-none focus:ring-violet-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-violet-600 dark:hover:bg-violet-700 dark:focus:ring-violet-800',
 };
