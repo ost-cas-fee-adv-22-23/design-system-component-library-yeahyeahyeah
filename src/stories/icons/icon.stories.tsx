9a4417e0c1815b5b7901b4cc9b6d2fb686c4c828
Thomas Schallert <thomas.schallert@fhnw.ch>
2022-11-29 15:55:05 +0100

    render icons dynamically in icon story

--- src/stories/icons/icon.stories.tsx
+++ src/stories/icons/icon.stories.tsx
@@ -8,87 +8,49 @@ export default {
   component: Icon,
 } as ComponentMeta<typeof Icon>;
 
+const iconTypes = [
+  'calendar',
+  'cancel',
+  'checkmark',
+  'chevron-up',
+  'chevron-down',
+  'chevron-left',
+  'chevron-right',
+  'edit',
+  'eye',
+  'fullscreen',
+  'heart-filled',
+  'heart-outlined',
+  'location',
+  'logout',
+  'mumble',
+  'profile',
+  'reply-filled',
+  'reply-outlined',
+  'repost',
+  'send',
+  'settings',
+  'share',
+  'time',
+  'upload',
+];
+
+let tailWindColors = ['fill-slate-500', 'fill-violet-500', 'fill-pink-500'];
+let tailWindColorsElement: string = '';
+
 const TemplateIcon: ComponentStory<typeof Icon> = (args) => (
   <>
+    {iconTypes.map((iconType, index) => {
+      tailWindColorsElement = tailWindColors.shift() || '';
+      let icon = (
+        <Icon {...args} type={iconType} color={tailWindColorsElement} />
+      );
+      tailWindColors.push(tailWindColorsElement);
+      return <p key={index}>{icon}</p>;
+    })}
+
     <p>
       <Icon {...args} />
-    </p>
-    <p>
-      <Icon
-        {...args}
-        type="calendar"
-        color="fill-slate-500"
-        fCallBack={() => console.log('calendar clicked')}
-      />
-    </p>
-    <p>
-      <Icon {...args} type="cancel" color="fill-violet-500" />
-    </p>
-    <p>
-      <Icon {...args} type="checkmark" color="fill-pink-500" />
-    </p>
-    <p>
-      <Icon {...args} type="chevron-up" color="fill-slate-500" />
-    </p>
-    <p>
-      <Icon {...args} type="chevron-down" color="fill-violet-500" />
-    </p>
-    <p>
-      <Icon {...args} type="chevron-left" color="fill-pink-500" />
-    </p>
-    <p>
-      <Icon {...args} type="chevron-right" color="fill-slate-500" />
-    </p>
-    <p>
-      <Icon {...args} type="edit" color="fill-violet-500" />
-    </p>
-    <p>
-      <Icon {...args} type="eye" color="fill-pink-500" />
-    </p>
-    <p>
-      <Icon {...args} type="fullscreen" color="fill-slate-500" />
-    </p>
-    <p>
-      <Icon {...args} type="heart-filled" color="fill-violet-500" />
-    </p>
-    <p>
-      <Icon {...args} type="heart-outlined" color="fill-pink-500" />
-    </p>
-    <p>
-      <Icon {...args} type="location" color="fill-slate-500" />
-    </p>
-    <p>
-      <Icon {...args} type="logout" color="fill-violet-500" />
-    </p>
-    <p>
-      <Icon {...args} type="mumble" color="fill-pink-500" />
-    </p>
-    <p>
-      <Icon {...args} type="profile" color="fill-slate-500" />
-    </p>
-    <p>
-      <Icon {...args} type="reply-filled" color="fill-violet-500" />
-    </p>
-    <p>
-      <Icon {...args} type="reply-outlined" color="fill-pink-500" />
-    </p>
-    <p>
-      <Icon {...args} type="repost" color="fill-slate-500" />
-    </p>
-    <p>
-      <Icon {...args} type="send" color="fill-violet-500" />
-    </p>
-    <p>
-      <Icon {...args} type="settings" color="fill-pink-500" />
-    </p>
-    <p>
-      <Icon {...args} type="share" color="fill-slate-500" />
-    </p>
-    <p>
-      <Icon {...args} type="time" color="fill-violet-500" />
-    </p>
-    <p>
-      <Icon {...args} type="upload" color="fill-pink-500" />
     </p>
   </>
 );
@@ -97,5 +59,5 @@ export const IconCancel = TemplateIcon.bind({});
 IconCancel.args = {
   type: 'upload',
   color: 'fill-slate-300',
-  fCallBack: (e) => console.log(`${e.target} clicked`),
+  fCallBack: () => console.log('icon clicked'),
 };
