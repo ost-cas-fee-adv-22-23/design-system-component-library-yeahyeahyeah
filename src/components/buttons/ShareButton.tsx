9b5d50ab24d317791c150a974a95cccd4c26c58b
webrooster <roland.vonaesch@fhnw.ch>
2022-12-06 10:28:41 +0100

    SB ShareButton - link copied status

--- src/components/buttons/ShareButton.tsx
+++ src/components/buttons/ShareButton.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled, { css } from 'styled-components';
-import tw, { theme } from 'twin.macro';
+import tw from 'twin.macro';
 import { Icon, IIcon } from 'src/components/Icon';
 
 interface IButtonProps extends React.HtmlHTMLAttributes<HTMLButtonElement> {
@@ -10,15 +10,24 @@ interface IButtonProps extends React.HtmlHTMLAttributes<HTMLButtonElement> {
 
 export const ShareButton: React.FC<IButtonProps & IIcon> = ({
   label = 'Copy Link',
-  handleClick,
   iconName = 'share',
   iconColor = 'fill-slate-600',
 }) => {
+  const [labelText, setLabelText] = useState<string>(label);
+
+  const handleClick = () => {
+    setLabelText('Link copied');
+
+    window.setTimeout(() => {
+      setLabelText(label);
+    }, 1000);
+  };
+
   return (
     <>
       <ButtonStyles onClick={handleClick}>
         <Icon iconName={iconName} iconColor={iconColor} />
-        {label}
+        {labelText}
       </ButtonStyles>
     </>
   );
@@ -28,34 +37,27 @@ export const ShareButton: React.FC<IButtonProps & IIcon> = ({
  * @Button
  * @desc Button styles
  */
-const buttonFont = tw`
-  text-skin-light
-  font-semibold
-  leading-normal
-`;
+const ButtonStyles = styled.button(() => [
+  tw`
+    text-skin-light
+    font-semibold
+    leading-normal
+    text-slate-600
 
-const buttonDefaults = tw`
-  flex
-  grow-0
-  justify-center
-  items-center
-  p-12
-  rounded-full
-  w-auto
-  outline-none
-  text-slate-600
-  bg-none
-  hover:(text-slate-700 bg-slate-100)
-  focus:(text-slate-700)
-`;
-
-const ButtonStyles = styled.button(({}) => [
-  buttonFont,
-  buttonDefaults,
-  // css({
-  //   color: theme`colors.slate.600`,
-  //   svg: theme`colors.slate.100`,
-  // }),
+    flex
+    grow-0
+    justify-center
+    items-center
+    p-12
+    rounded-full
+    
+    w-auto
+    outline-none
+    bg-none
+    
+    hover:(text-slate-700 bg-slate-100)
+    focus:(text-slate-700)
+  `,
   css`
     svg {
       margin-left: 0;
