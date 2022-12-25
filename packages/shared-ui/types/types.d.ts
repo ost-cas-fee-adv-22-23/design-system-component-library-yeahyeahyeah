05d4d6bb877f1ccac6ef5f6d93e24172a0bdf668
Thomas Schallert <thomas.schallert@fhnw.ch>
2022-12-25 17:06:11 +0100

    extend tsconfig

--- packages/shared-ui/types/types.d.ts
+++ packages/shared-ui/types/types.d.ts
@@ -19,3 +19,12 @@ declare module 'react' {
     tw?: string;
   }
 }
+
+// The 'as' prop on styled components
+declare global {
+  namespace JSX {
+    interface IntrinsicAttributes<T> extends DOMAttributes<T> {
+      as?: string;
+    }
+  }
+}
