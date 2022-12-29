c8920f706678836980be11ca53747ec400c68222
Thomas Schallert <thomas.schallert@fhnw.ch>
2022-12-29 19:12:39 +0100

    refactor button

--- packages/shared-ui/components/buttons/Button.tsx
+++ packages/shared-ui/components/buttons/Button.tsx
@@ -73,46 +73,41 @@ const buttonFocus = tw`
   focus:(outline-4 outline-offset-0)
 `;
 
-const variantSlate = tw`
+const Variant = ({ variant }: ButtonProps) => [
+  tw`
+  !bg-slate-600
+  hover:(bg-slate-700 outline-slate-100)
+  focus:(outline-slate-200)
+  disabled:bg-slate-300`,
+  variant === 'slate' &&
+    tw`
   !bg-slate-600
   hover:(bg-slate-700 outline-slate-100)
   focus:(outline-slate-200)
   disabled:bg-slate-300
-`;
-
-const variantViolet = tw`
+`,
+  variant === 'violet' &&
+    tw`
   !bg-violet-600
   hover:(bg-violet-700 outline-violet-100)
   focus:(outline-violet-200)
   disabled:bg-violet-300
-`;
-
-const variantPink = tw`
+`,
+  variant === 'pink' &&
+    tw`
   !bg-gradient-to-r from-pink-500 to-violet-500
   hover:(bg-gradient-to-r-70 outline-violet-100)
   focus:(bg-gradient-to-r-80 outline-violet-200)
   disabled:(bg-gradient-to-r)
-`;
+`,
+];
 
-const buttonWidth = (width: string | undefined) => [
+const ButtonWidth = ({ width }: ButtonProps) => [
   width === 'default' ? null : tw`w-full`,
   width === 'large' && tw`w-[184px] h-[40px]`,
 ];
 
-const buttonSize = (size: string | undefined) => (size === 'large' ? tw`py-16 px-24` : null);
-
-const getVariant = (variant: string | undefined) => {
-  switch (variant) {
-    case 'slate':
-      return variantSlate;
-    case 'violet':
-      return variantViolet;
-    case 'pink':
-      return variantPink;
-    default:
-      return variantSlate;
-  }
-};
+const ButtonSize = ({ size }: ButtonProps) => size === 'large' && tw`py-16 px-24`;
 
 const IconMargin = css`
   svg {
@@ -120,13 +115,13 @@ const IconMargin = css`
   }
 `;
 
-const StyledButton = styled.button(({ variant, width, size }: ButtonProps) => [
+const StyledButton = styled.button(() => [
   buttonFont,
   buttonDefaults,
   buttonHover,
   buttonFocus,
-  buttonWidth(width),
-  buttonSize(size),
-  getVariant(variant),
+  ButtonWidth,
+  ButtonSize,
+  Variant,
   IconMargin,
 ]);
