3851a823a3b05ee7da0a7afb826d5795f727daad
Thomas Schallert <thomas.schallert@fhnw.ch>
2023-01-07 21:32:02 +0100

    fix(Mumble): add missing type to Mumble, add nice timeline template to next-app, add babel-plugin-twin for automatic twin import

--- packages/app/pages/index.tsx
+++ packages/app/pages/index.tsx
@@ -1,83 +1,267 @@
 import React from 'react';
 import {
-  Logo,
-  Cancel,
-  Calendar,
-  Checkmark,
-  LogoMumble,
-  Button,
+  Navigation,
   Mumble,
+  WriteComponent,
+  Heading,
+  Container,
 } from '@smartive-education/design-system-component-library-yeahyeahyeah';
-import tw from 'twin.macro';
 
-const App = () => {
+export default function Home() {
   const handleAvatar = () => {
-    console.log('avatar');
+    console.log('avatar clicked');
   };
-  return (
-    <div>
-      <Button variant="pink" size="large" label="Roli Chicken Fest" icon="logo" width="default" />
-      <Logo width={200} height={200} css={tw`fill-violet-600`} />
-      <LogoMumble width={200} height={200} css={tw`fill-violet-600`} />
-      <Calendar width={100} height={100} css={tw`fill-violet-600`} />
-      <Cancel width={100} height={100} css={tw`fill-violet-600`} />
-      <Checkmark width={100} height={100} css={tw`fill-violet-600`} />
-      <p css={tw`text-pink-600 text-3xl`}>Hello World</p>
-      <p tw={'text-violet-600 text-3xl'}>Hello World</p>
-      <Mumble
-        variant="detailpage"
-        comment={{
-          fCallBack: () => console.log('comment'),
-          quantity: 0,
-        }}
-        img={{
-          alt: 'This is a profile picture!',
-          fCallBack: function noRefCheck() {
-            return null;
-          },
-          src: 'https://shorturl.at/uvyKU',
-        }}
-        like={{
-          fCallBack: () => console.log('like'),
-          quantity: 999,
-        }}
-        share={{
-          fCallBack: () => console.log('share'),
-          label: 'Share',
-        }}
-        text="Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking."
-        user={{
-          avatar: {
-            alt: 'avatar',
-            imageCallBack: handleAvatar,
-            src: 'https://media.giphy.com/media/ZYzt9dXQUjmBa/giphy.gif',
-          },
-          joined: {
-            href: '',
-            label: 'Joined',
-            fCallBack: () => console.log('joined'),
-          },
-          label: 'Display Name',
-          location: {
-            href: '',
-            label: 'Location',
-            fCallBack: () => console.log('location'),
-          },
-          timestamp: {
-            href: '',
-            label: 'Timestamp',
-            fCallBack: () => console.log('timestamp'),
-          },
-          username: {
-            href: '',
-            label: 'Username',
-            fCallBack: () => console.log('username'),
-          },
-          variant: 'xlarge',
-        }}
-      />
-    </div>
-  );
-};
 
-export default App;
+  return (
+    <>
+      <div tw="flex flex-col justify-center items-start bg-slate-200 w-full h-full">
+        <div tw="w-full mb-32">
+          <Navigation
+            logo={{
+              title: 'Mumble Logo',
+              href: '#',
+              variant: 'white',
+              alignment: 'horizontal',
+              fCallBack: () => console.log('logo'),
+              isNavigation: true,
+            }}
+            avatar={{
+              label: 'Label',
+              variant: 'profile',
+              fCallBack: handleAvatar,
+              src: 'https://media.giphy.com/media/cfuL5gqFDreXxkWQ4o/giphy.gif',
+            }}
+            settings={{
+              label: 'Detailview',
+              variant: 'default',
+              fCallBack: () => console.log('settings'),
+              icon: 'settings',
+            }}
+            logout={{
+              label: 'Logout',
+              variant: 'default',
+              fCallBack: () => console.log('logout'),
+              icon: 'logout',
+            }}
+          />
+        </div>
+        <Container layout="plain">
+          <Heading label="Willkommen auf Mumble" color="violet" tag="h2" size="default" mbSpacing="32" />
+          <Heading
+            label="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna."
+            color="light"
+            tag="h4"
+            size="default"
+            mbSpacing="32"
+          />
+        </Container>
+        <Container layout="plain">
+          <div tw="mb-32">
+            <WriteComponent
+              variant="write"
+              mbSpacing="64"
+              form={{
+                editType: 'textarea',
+                autoComplete: 'off',
+                required: false,
+                errorMessage: 'Da ist etwas schief gelaufen',
+                placeholder: 'Na, was meinste dazu ...?',
+              }}
+              send={{
+                fCallBack: () => {
+                  console.log('clicked');
+                },
+                icon: 'send',
+                label: 'Absenden',
+                size: 'small',
+                type: 'button',
+                variant: 'violet',
+                width: 'full',
+              }}
+              setText={() => {
+                return null;
+              }}
+              startHeading="Hey, was läuft?"
+              upload={{
+                fCallBack: () => {
+                  console.log('clicked');
+                },
+                icon: 'upload',
+                label: 'Bild hochladen',
+                size: 'small',
+                type: 'button',
+                variant: 'slate',
+                width: 'full',
+              }}
+              user={{
+                avatar: {
+                  alt: 'Family Guy goes Mumble',
+                  src: 'https://media.giphy.com/media/ZYzt9dXQUjmBa/giphy.gif',
+                },
+                label: 'Hey, was läuft?',
+                username: {
+                  href: '#',
+                  label: 'Username',
+                  type: 'username',
+                },
+                variant: 'recommended',
+              }}
+            />
+          </div>
+          <Mumble
+            comment={{
+              fCallBack: () => {
+                console.log('clicked');
+              },
+              quantity: 0,
+            }}
+            img={{
+              alt: 'This is a profile picture!',
+              fCallBack: () => {
+                console.log('clicked');
+              },
+              src: 'https://shorturl.at/nEO01',
+            }}
+            like={{
+              fCallBack: () => {
+                console.log('clicked');
+              },
+              quantity: 999,
+            }}
+            mbSpacing="32"
+            share={{
+              fCallBack: () => {
+                console.log('clicked');
+              },
+              label: 'Share',
+            }}
+            text="Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking."
+            user={{
+              avatar: {
+                alt: 'avatar',
+                buttonCallBack: () => {
+                  console.log('clicked');
+                },
+                imageCallBack: () => {
+                  console.log('clicked');
+                },
+                src: 'https://media.giphy.com/media/cfuL5gqFDreXxkWQ4o/giphy.gif',
+              },
+              joined: {
+                type: 'joined',
+                fCallBack: () => {
+                  console.log('clicked');
+                },
+                href: '',
+                label: 'Joined',
+              },
+              label: 'Display Name',
+              location: {
+                type: 'joined',
+                fCallBack: () => {
+                  console.log('clicked');
+                },
+                href: '',
+                label: 'Location',
+              },
+              timestamp: {
+                type: 'joined',
+                fCallBack: () => {
+                  console.log('clicked');
+                },
+                href: '',
+                label: 'Timestamp',
+              },
+              username: {
+                type: 'joined',
+                fCallBack: () => {
+                  console.log('clicked');
+                },
+                href: '',
+                label: 'Username',
+              },
+              variant: 'xlarge',
+            }}
+            variant="timeline"
+          />
+          <Mumble
+            comment={{
+              fCallBack: () => {
+                console.log('clicked');
+              },
+              quantity: 0,
+            }}
+            img={{
+              alt: 'This is a profile picture!',
+              fCallBack: () => {
+                console.log('clicked');
+              },
+              src: 'https://shorturl.at/nEO01',
+            }}
+            like={{
+              fCallBack: () => {
+                console.log('clicked');
+              },
+              quantity: 999,
+            }}
+            mbSpacing="32"
+            share={{
+              fCallBack: () => {
+                console.log('clicked');
+              },
+              label: 'Share',
+            }}
+            text="Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking."
+            user={{
+              avatar: {
+                alt: 'avatar',
+                buttonCallBack: () => {
+                  console.log('clicked');
+                },
+                imageCallBack: () => {
+                  console.log('clicked');
+                },
+                src: 'https://media.giphy.com/media/cfuL5gqFDreXxkWQ4o/giphy.gif',
+              },
+              joined: {
+                type: 'joined',
+                fCallBack: () => {
+                  console.log('clicked');
+                },
+                href: '',
+                label: 'Joined',
+              },
+              label: 'Display Name',
+              location: {
+                type: 'joined',
+                fCallBack: () => {
+                  console.log('clicked');
+                },
+                href: '',
+                label: 'Location',
+              },
+              timestamp: {
+                type: 'joined',
+                fCallBack: () => {
+                  console.log('clicked');
+                },
+                href: '',
+                label: 'Timestamp',
+              },
+              username: {
+                type: 'joined',
+                fCallBack: () => {
+                  console.log('clicked');
+                },
+                href: '',
+                label: 'Username',
+              },
+              variant: 'xlarge',
+            }}
+            variant="timeline"
+          />
+        </Container>
+      </div>
+    </>
+  );
+}
