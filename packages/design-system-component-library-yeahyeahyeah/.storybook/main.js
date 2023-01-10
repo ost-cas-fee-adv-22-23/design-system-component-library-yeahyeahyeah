4ea48a2446e32527b02a457da676c3bb6fa34ba1
Thomas Schallert <thomas.schallert@fhnw.ch>
2023-01-10 12:46:11 +0100

    remove postcss config, because we use global styles from twin.macro

--- packages/design-system-component-library-yeahyeahyeah/.storybook/main.js
+++ packages/design-system-component-library-yeahyeahyeah/.storybook/main.js
@@ -2,20 +2,5 @@ const path = require('path');
 
 module.exports = {
   stories: ['../**/*.stories.mdx', '../**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-a11y',
-    {
-      name: '@storybook/addon-postcss',
-      options: {
-        cssLoaderOptions: {
-          importLoaders: 1,
-        },
-        postcssLoaderOptions: {
-          implementation: require('postcss'),
-        },
-      },
-    },
-  ],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-a11y'],
 };
