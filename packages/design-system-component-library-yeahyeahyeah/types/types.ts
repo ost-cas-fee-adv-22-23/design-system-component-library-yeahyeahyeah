d632fc5ea3b7d83215ebf98afc7a8b4006a517b3
webrooster <roland.vonaesch@fhnw.ch>
2023-01-11 15:39:03 +0100

    feat(Heading/Types): design token types

--- packages/design-system-component-library-yeahyeahyeah/types/types.ts
+++ packages/design-system-component-library-yeahyeahyeah/types/types.ts
@@ -1,1 +1,5 @@
 export type TmbSpacing = '0' | '2' | '4' | '8' | '16' | '32' | '64';
+export type TmumbleColors = 'slate' | 'violet' | 'pink' | 'white' | 'medium' | 'dark' | 'light' | 'default';
+export type TSizes = 'default' | 'xlarge' | 'large' | 'medium' | 'small';
+export type THeadingTags = 'h1' | 'h2' | 'h3' | 'h4' | 'span';
+export type TAlignment = 'left' | 'center' | 'right' | 'default';
