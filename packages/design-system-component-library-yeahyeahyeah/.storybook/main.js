8bf6d9df7ba15d00ce8edbd6b76bff3951c4189b
webrooster <roland.vonaesch@fhnw.ch>
2023-01-12 16:14:16 +0100

    feat(Welcome): welcome/guideline page

--- packages/design-system-component-library-yeahyeahyeah/.storybook/main.js
+++ packages/design-system-component-library-yeahyeahyeah/.storybook/main.js
@@ -3,15 +3,5 @@ const path = require('path');
 module.exports = {
   stories: ['../**/*.stories.mdx', '../**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-a11y'],
-  staticDirs: ['../stories/assets/'],
-  rules: [
-    {
-      test: /\.(png|jpe?g|gif)$/i,
-      use: [
-        {
-          loader: 'file-loader',
-        },
-      ],
-    },
-  ],
+  stories: ['../stories/Intro.stories.tsx', '../**/*.stories.tsx'],
 };
