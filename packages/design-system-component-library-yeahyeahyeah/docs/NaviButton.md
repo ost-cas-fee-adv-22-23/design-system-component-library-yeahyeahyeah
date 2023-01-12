432b529d0227710aa5a9dfdf58904815c50dcdb9
webrooster <roland.vonaesch@fhnw.ch>
2023-01-12 14:14:53 +0100

    feat(NaviButton/Readme): cleanup, split story

--- packages/design-system-component-library-yeahyeahyeah/docs/NaviButton.md
+++ packages/design-system-component-library-yeahyeahyeah/docs/NaviButton.md
@@ -3,11 +3,11 @@
 ## NaviButton properties
 | Property|Description|
 |-|-|
-|label|Label of NaviButton. Remarks: *label* not visible on small devices.|
-|variant|Image source link.|
-|alt|Alternative tag for image.|
-|buttonCallBack|JS-Callback for button. Exclusive only in *edit* variant.|
-|imageCallBack|JS-Callback for image.|
+|label|Label of NaviButton. Remarks: *label* not visible on small devices, but present in DOM.|
+|variant|Use *profile* for [Navigation Profile Button](./?path=/story/navigation-navigation--navigation-story). Use *default* for all other navigation links. For variant *profile* see [Avatar](./?path=/docs/user--avatar-story) and choose variant *small*.|
+|fCallBack|JS-Callback function.|
+|avatar|Only relevant for variant *profile*. See settings in [Avatar](./?path=/docs/user--avatar-story).|
+|icon|Choose icon accordingly. See details on [Icons](./?path=/docs/icons-collection--all).|
 
 ## Include NaviButton from the component library
 
@@ -16,9 +16,7 @@
 
 import { NaviButton } from "@smartive-education/design-system-component-library-yeahyeahyeah"
 ```
-
-### NaviButton *small* example
-
+### NaviButton *default* example
 ```js
 
 <NaviButton
@@ -26,6 +24,22 @@ import { NaviButton } from "@smartive-education/design-system-component-library-
   icon="settings"
   label="Label"
   variant="default"
+/>
+
+```
+
+### NaviButton *profile* example
+```js
+
+<NaviButton
+  avatar={{
+    alt: 'Alternative text',
+    src: 'https://media.giphy.com/media/cfuL5gqFDreXxkWQ4o/giphy.gif',
+    variant: 'small'
+  }}
+  fCallBack={() => {}}
+  label="Label"
+  variant="profile"
 />
 
 ```
--- packages/design-system-component-library-yeahyeahyeah/stories/buttons/NaviButton.stories.tsx
+++ packages/design-system-component-library-yeahyeahyeah/stories/buttons/NaviButton.stories.tsx
@@ -9,40 +9,20 @@ export default {
   component: NaviButton,
   argTypes: {
     label: {
-      name: 'label',
-      defaultValue: 'Label',
+      control: 'text',
     },
     variant: {
-      control: {
-        type: 'select',
-      },
+      control: 'select',
     },
     fCallBack: {
       action: () => 'handleClick',
     },
-    width: {
-      control: false,
-      table: {
-        disable: true,
-      },
-    },
-    size: {
-      control: false,
-      table: {
-        disable: true,
-      },
-    },
-    disabled: {
-      control: {
-        type: 'boolean',
-      },
-      defaultValue: false,
-    },
     icon: {
       control: 'select',
       options: Object.keys(IconsMapped),
-      name: 'Icon',
-      defaultValue: 'settings',
+    },
+    avatar: {
+      control: 'object',
     },
   },
 } as ComponentMeta<typeof NaviButton>;
@@ -50,15 +30,45 @@ export default {
 const Template: ComponentStory<typeof NaviButton> = (args) => {
   return <NaviButton {...args} />;
 };
-/**
- * @button
- * @desc button standard slate
- */
+export const ProfileButtonStory = Template.bind({});
+
+// PROFILE BUTTON
+ProfileButtonStory.argTypes = {
+  icon: {
+    control: false,
+  },
+};
+
+ProfileButtonStory.args = {
+  label: 'Label',
+  variant: 'profile',
+  avatar: {
+    variant: 'small',
+    src: 'https://media.giphy.com/media/cfuL5gqFDreXxkWQ4o/giphy.gif',
+    alt: 'Alternative text',
+  },
+};
+
+ProfileButtonStory.parameters = {
+  docs: {
+    source: { type: 'dynamic' },
+    description: {
+      component: NaviButtonReadme,
+    },
+  },
+};
+
+ProfileButtonStory.storyName = 'Profil Button';
+
 export const NaviButtonStory = Template.bind({});
+
+// NAVI BUTTON
+NaviButtonStory.argTypes = {};
 
 NaviButtonStory.args = {
   label: 'Label',
   variant: 'default',
+  icon: 'settings',
 };
 
 NaviButtonStory.parameters = {
