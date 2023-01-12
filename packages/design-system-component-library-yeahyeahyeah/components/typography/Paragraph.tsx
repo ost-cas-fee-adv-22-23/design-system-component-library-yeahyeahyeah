a696da510de313873babcf62afdc7123aa4c8e6b
webrooster <roland.vonaesch@fhnw.ch>
2023-01-12 11:08:42 +0100

    fix(Paragraph): size, color, alignment not req.

--- packages/design-system-component-library-yeahyeahyeah/components/typography/Paragraph.tsx
+++ packages/design-system-component-library-yeahyeahyeah/components/typography/Paragraph.tsx
@@ -5,19 +5,13 @@ import type { TmbSpacing } from '../../types/types';
 
 export interface IParagraphProps {
   text: string;
-  size: 'default' | 'medium' | 'large';
-  color: 'default' | 'dark' | 'light' | 'white';
-  alignment: 'left' | 'center' | 'right';
+  size?: 'default' | 'medium' | 'large';
+  color?: 'default' | 'dark' | 'light' | 'white';
+  alignment?: 'left' | 'center' | 'right';
   mbSpacing?: TmbSpacing;
 }
 
-export const Paragraph: React.FC<IParagraphProps> = ({
-  size = 'medium',
-  color = 'default',
-  mbSpacing,
-  text,
-  alignment = 'left',
-}) => {
+export const Paragraph: React.FC<IParagraphProps> = ({ size = 'medium', color = 'default', mbSpacing, text, alignment }) => {
   return (
     <>
       <ParagraphStyles size={size} color={color} mbSpacing={mbSpacing} alignment={alignment}>
@@ -35,13 +29,16 @@ interface IParagraphStylesProps {
 }
 
 const Aligment = ({ alignment }: IParagraphStylesProps) => [
+  tw`
+    text-left
+  `,
   alignment === 'left' && tw`text-left`,
   alignment === 'center' && tw`text-center`,
   alignment === 'right' && tw`text-right`,
 ];
 
 const paragraphDefaults = tw`font-medium w-full`;
-const paragraphMedium = tw`text-base font-medium`; // leading-loose
+const paragraphMedium = tw`text-base font-medium`;
 const paragraphLarge = tw`text-lg font-medium`;
 const paragraphColorDark = tw`text-slate-900`;
 const paragraphColorLight = tw`text-slate-500`;
