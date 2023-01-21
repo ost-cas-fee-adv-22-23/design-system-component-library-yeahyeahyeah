5c999dafc557c5d788c8db68378e462c31f20dbd
webrooster <roland.vonaesch@fhnw.ch>
2023-01-21 11:18:51 +0100

    fix(Storybook): use addon postcss

--- packages/design-system-component-library-yeahyeahyeah/.storybook/main.js
+++ packages/design-system-component-library-yeahyeahyeah/.storybook/main.js
@@ -2,5 +2,11 @@ const path = require('path');
 
 module.exports = {
   stories: ['../**/*.stories.@(js|jsx|ts|tsx|mdx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-a11y', '@storybook/addon-docs'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-a11y',
+    '@storybook/addon-docs',
+    '@storybook/addon-postcss',
+  ],
 };
