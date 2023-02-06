0742b9d45de3de69c8bd0fb6f647ea0b9c8de1b5
Thomas Schallert <thomas.schallert@fhnw.ch>
2023-02-06 11:40:50 +0100

    feature(Upload): create new release

--- packages/design-system-component-library-yeahyeahyeah/components/modal/Modal.tsx
+++ packages/design-system-component-library-yeahyeahyeah/components/modal/Modal.tsx
@@ -150,7 +150,6 @@ const IconWrapper = styled.div(() => [
     justify-center
     items-center
     transition ease-in-out delay-150
-    
     hover:(rotate-90 transform-gpu)
     cursor-pointer
   `,
