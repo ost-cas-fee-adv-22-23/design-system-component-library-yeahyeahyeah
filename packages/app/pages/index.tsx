e25eb746f0925ee3ffeeca877bf8949e15d61192
Thomas Schallert <thomas.schallert@fhnw.ch>
2023-02-08 09:26:45 +0100

    add upload forms to example pages

--- packages/app/pages/index.tsx
+++ packages/app/pages/index.tsx
@@ -1,12 +1,35 @@
 import React, { useEffect, useMemo, useState } from 'react';
 import Navi from './includes/navi';
-import { Mumble, TextBox, Heading, Container } from '@smartive-education/design-system-component-library-yeahyeahyeah';
+import {
+  Mumble,
+  TextBox,
+  Heading,
+  Container,
+  Modal,
+  FileUpload,
+  Button,
+} from '@smartive-education/design-system-component-library-yeahyeahyeah';
 import debounce from 'lodash.debounce';
+import { FileRejection } from 'react-dropzone';
+import tw, { styled } from 'twin.macro';
 
 export default function Timeline() {
   const [posts, setPosts] = useState(['']);
   const [inputValue, setInputValue] = useState<string>('');
   const [errorMessage, setErrorMessage] = useState<string>('');
+  const [showModal, setShowModal] = useState(false);
+  const [fileUploadError, setFileUploadError] = useState('');
+
+  const setTimerForError = () =>
+    setTimeout(() => {
+      setFileUploadError('');
+    }, 2000);
+
+  const onDropCallBack = (acceptedFiles: File[], fileRejections: FileRejection[]) => {
+    console.log('acceptedFiles, fileRejections', acceptedFiles, fileRejections);
+    fileRejections?.length && setFileUploadError(fileRejections[0].errors[0].message);
+    setTimerForError();
+  };
 
   const setErrorDebounced = useMemo(
     () =>
@@ -38,11 +61,43 @@ export default function Timeline() {
   };
 
   const handleUpload = () => {
-    console.log('upload');
+    setShowModal(true);
   };
 
   return (
     <>
+      <Modal label={'Modal'} isOpen={showModal} wide={true} onClose={() => setShowModal(false)}>
+        <form onSubmit={() => console.log('Submit')} tw="container">
+          <FileUpload
+            label="Datei hierhin ziehen ..."
+            fileDescription="JPEG oder PNG, maximal 50 MB"
+            dragDescription="Jetzt loslassen ..."
+            loading={false}
+            onDropCallBack={onDropCallBack}
+            errorMessage={fileUploadError}
+          />
+          <Row>
+            <Button
+              fCallBack={() => setShowModal(false)}
+              icon="cancel"
+              label="Abbrechen"
+              size="large"
+              type="button"
+              color="slate"
+              width="full"
+            />
+            <Button
+              fCallBack={() => console.log('Send Button clicked')}
+              icon="send"
+              label="Speichern"
+              size="small"
+              type="button"
+              color="violet"
+              width="full"
+            />
+          </Row>
+        </form>
+      </Modal>
       <div tw="flex flex-col justify-center items-center bg-slate-200 w-full h-full pb-64">
         <Navi />
         <Container layout="plain">
@@ -150,3 +205,19 @@ export default function Timeline() {
     </>
   );
 }
+
+export interface IRowStyle {
+  upload?: string;
+}
+
+const Row = styled.div(({ upload }: IRowStyle) => [
+  tw`
+    flex
+    justify-between
+    gap-16
+    flex-col
+    sm:(flex-row)
+    mt-48
+  `,
+  upload === 'upload' && tw`mt-16`,
+]);
