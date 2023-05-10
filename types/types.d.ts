a73a472359e743ed1fb51df25cf424c5c5acfe34
Thomas Schallert <thomas.schallert@fhnw.ch>
2023-05-10 16:47:27 +0200

    fix: remove unused types for styled components

--- types/types.d.ts
+++ types/types.d.ts
@@ -21,12 +21,3 @@ declare module 'react' {
     tw?: string;
   }
 }
-
-// The 'as' prop on styled components
-declare global {
-  namespace JSX {
-    interface IntrinsicAttributes<T> extends DOMAttributes<T> {
-      as?: string;
-    }
-  }
-}
